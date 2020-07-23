b7f6af10fbecefee18703aa3123a6847b6890b2f
Mertcan Arguc <arguc.mertcan@gmail.com>
2020-07-23 21:10:17 +0300

    çjasdbjkasdbkjd

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,11 @@ router.get('/', function(req, res, next) {
   res.render('front/index', { title: 'İlan Arıyor' });
 });
 
+router.get("/contact",function(req,res,next){
+  res.render('front/main/contact',{
+    title:"İletişim ve Destek"
+  })
+})
 
 
 module.exports = router;
